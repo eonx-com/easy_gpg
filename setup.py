9e393335e60497a23fa8699bba3df52e67a3fafe
Damian Sloane <dddaaammmooo@hotmail.com>
2020-02-17 15:49:44 +1100

    Interim commit, work in progress...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EasyGpg',
-    version='2.0.0',
+    version='1.0',
     description='Python service to GPG encrypt files',
     url='git@github.com:eonx-com/EasyGpg',
     author='Damian Sloane',
